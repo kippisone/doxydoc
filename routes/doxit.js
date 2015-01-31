2a0a616a3328915d7955222fe15cce6370abc251
AndiOxidant <andi.oxidant@noname-media.com>
2015-01-31 10:08:08 +0100

    Removed old stuff

--- routes/doxit.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app, callback) {
-	'use strict';
-	
-	app.addRoute('get', '/data', 'Get latest modules', function(req, res) {
-		res.sendfile('docs/doxit-latest.json');
-	});
-
-	callback();
-};
